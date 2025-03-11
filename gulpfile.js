dbd15f95fe8499652a904dbe708584c61ba85b97
Rd-rick <rick0202.contato@gmail.com>
2025-03-11 11:25:12 -0300

    Criando o ero e importando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require ('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imageMin = require('gulp-imagemin')
 
 function compilaSass() {
     return gulp.src('./src/styles/*.scss')
@@ -7,6 +8,14 @@ function compilaSass() {
         .pipe(gulp.dest('./dist/css'))
 }
 
+function minificaImages() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest('./dist/images'))
+}
+
 exports.default = function() {
     gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, compilaSass)
 }
+
+exports.minificaImages = gulp.parallel(compilaSass, minificaImages)
